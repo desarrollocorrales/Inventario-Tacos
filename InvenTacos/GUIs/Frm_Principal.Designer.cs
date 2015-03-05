ea763d3b7da382172f12ac64e15d11c949625c8c
desarrollocorrales <desarrollo@loscorrales.com.mx>
2015-03-05 12:09:43 -0600

    Creado el proyecto
    Primer Commit al proyecto de control de inventarios para tacos

---
+++ InvenTacos/GUIs/Frm_Principal.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace InvenTacos.GUIs
+{
+    partial class Frm_Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Frm_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 573);
+            this.Name = "Frm_Principal";
+            this.Text = "Inventario Tacos Los Corrales";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
